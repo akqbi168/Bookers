893b95ebeb2650f9b8c31dd22f7ea5b0e56d5e08
akqbi168 <akqbi168@gmail.com>
2019-01-15 14:15:06 +0900

    updated 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   # get 'books/show'
   # get 'books/index'
   # get 'books/new'
   # get 'books/edit'
+
   root "books#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :books
+
 end
