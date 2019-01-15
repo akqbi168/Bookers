c649c9fee6f36388a666c6a2df3339f076e36917
akqbi168 <akqbi168@gmail.com>
2019-01-15 12:53:31 +0900

    updated 2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
 	def show
+		@book = Book.find(params[:id])
 	end
 
 	def index
@@ -17,8 +18,20 @@ class BooksController < ApplicationController
 		redirect_to books_path
 	end
 
-
 	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		book = Book.find(params[:id])
+		book.update(blog_params)
+		redirect_to blog_path(blog)
+	end
+
+	def destroy
+		book = Book.find(params[:id])
+		book.destroy
+		redirect_to books_path
 	end
 
 	private
