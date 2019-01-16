bb351f4d80e5d0678e9d12e69c9fee43849d7be5
akqbi168 <akqbi168@gmail.com>
2019-01-16 11:52:42 +0900

    updated 6

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,18 @@
 class Book < ApplicationRecord
-	validates :title, presence: true
-	validates :body, presence: true
+	#Titleが入力されていない場合エラーを返す
+	# validates :title, presence: true
+	#Bodyが入力されていない場合エラーを返す
+	# validates :body, presence: true
+
+	validates :add_error_sample
+
+	def add_error_sample
+		if title.blank?
+			error[:base] << "Title項目が空欄です。"
+		end
+
+		if body.blank?
+			error[:base] << "Body項目が空欄です。"
+		end
+	end
 end
